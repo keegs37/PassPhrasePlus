88e474215c501167be5c155c0fea7f6dc4b2d77e
keegs37 <keegs37@example.com>
2025-09-21 13:07:35 -0700

    Added the conjugate function

--- conjugate.py
+++ conjugate.py
@@ -1,18 +1,7 @@
 from verb import Verb, create_verb
 from template import Template, create_template
 from subject import Subject, create_subject
-"""
-Tense,Aspect,Pattern
 
-x past,simple,[Subject] [Past]
-x future,simple,[Subject] will [Verb_Base]
-present,continuous,[Subject] am/are/is [Gerund]
-past,continuous,[Subject] was/were [Gerund]
-future,continuous,[Subject] will be [Gerund]
-present,perfect,[Subject] have/has [PastParticiple]
-past,perfect,[Subject] had [PastParticiple]
-future,perfect,[Subject] will have [PastParticiple]
-"""
 def conjugate(verb, template, subject):
     if template.tense == "past" and template.aspect == "simple":
        conjugated_pharse = template.pattern
@@ -39,6 +28,34 @@ def conjugate(verb, template, subject):
         if subject.person == "2" and subject.number == "singular":
             conjugated_pharse = conjugated_pharse.replace("am/are/is", "are")
         return conjugated_pharse
+    elif template.tense == "past" and template.aspect == "continuous":
+        conjugated_pharse = template.pattern
+        conjugated_pharse = conjugated_pharse.replace("[Subject]", subject.subject)
+        conjugated_pharse = conjugated_pharse.replace("[Gerund]", verb.gerund)
+        if subject.number == "singular":
+            conjugated_pharse = conjugated_pharse.replace("was/were", "was")
+        if subject.number == "plural":
+            conjugated_pharse = conjugated_pharse.replace("was/were", "were")
+        return conjugated_pharse
+    elif template.tense == "future" and template.aspect == "continuous":
+        conjugated_pharse = template.pattern
+        conjugated_pharse = conjugated_pharse.replace("[Subject]", subject.subject)
+        conjugated_pharse = conjugated_pharse.replace("[Gerund]", verb.gerund)
+        return conjugated_pharse
+    elif template.tense == "present" and template.aspect == "perfect":
+       conjugated_pharse = template.pattern
+       conjugated_pharse = conjugated_pharse.replace("[Subject]", subject.subject)
+       conjugated_pharse = conjugated_pharse.replace("[PastParticiple]", verb.past)
+       if subject.number == "singular":
+            conjugated_pharse = conjugated_pharse.replace("have/has", "has")
+       if subject.number == "plural":
+            conjugated_pharse = conjugated_pharse.replace("have/has", "have")
+       return conjugated_pharse
+    elif (template.tense == "past" or template.tense == "future") and template.aspect == "perfect":
+       conjugated_pharse = template.pattern
+       conjugated_pharse = conjugated_pharse.replace("[Subject]", subject.subject)
+       conjugated_pharse = conjugated_pharse.replace("[PastParticiple]", verb.past)
+       return conjugated_pharse
     
 verb = create_verb()
 template = create_template()
